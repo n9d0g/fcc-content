77bf45ef0c073549efd20db983646c8faee21719
n9d0g <nathanjlardizabal@gmail.com>
2023-03-28 22:08:56 -0400

    feat(schema): :sparkles: birthdays schema added

---
+++ schemas/birthdays.ts
@@ -0,0 +1,22 @@
+export default {
+  name: 'birthdays',
+  type: 'document',
+  title: 'Birthdays',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'birthday',
+      title: 'Birthday',
+      type: 'date',
+    },
+    {
+      name: 'pastor',
+      title: 'Pastor',
+      type: 'boolean',
+    },
+  ],
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import sermons from './sermons'
 import praise from './praise'
 import youngAdults from './young-adults'
+import birthdays from './birthdays'
 
-export const schemaTypes = [sermons, praise, youngAdults]
+export const schemaTypes = [sermons, praise, youngAdults, birthdays]
