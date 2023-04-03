7618580a93c6d32cac90801ab4eea84bd06107ab
n9d0g <nathanjlardizabal@gmail.com>
2023-04-03 12:56:15 -0400

    feat(schema): :sparkles: add new pages schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,5 +2,6 @@ import sermons from './sermons'
 import praise from './praise'
 import youngAdults from './young-adults'
 import birthdays from './birthdays'
+import pages from './pages'
 
-export const schemaTypes = [sermons, praise, youngAdults, birthdays]
+export const schemaTypes = [pages, sermons, praise, youngAdults, birthdays]
